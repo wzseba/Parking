d10eae2fa718e0d3e297bfa470f44c5656a5aab0
wzseba <wzseba@gmail.com>
2024-10-29 22:09:29 -0300

    add: metodo para obtener nombre del parking

--- src/parkingUno/Parking.java
+++ src/parkingUno/Parking.java
@@ -10,8 +10,16 @@ public class Parking {
 	private String nombreParking;
 
 	public Parking(int tamanio, String nombreParking) {
+		/*
+		 * Crea una lista inmutable con n copias de null y usa esa lista inmutable para
+		 * inicializar arraylist mutable
+		 */
 		this.matriculas = new ArrayList<String>(Collections.nCopies(tamanio, null));
 		this.nombreParking = nombreParking;
+	}
+
+	public String getNombre() {
+		return nombreParking;
 	}
 
 }
