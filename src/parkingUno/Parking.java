ecc9f75dd0fd300383e6d5d35e4c525889c448b4
wzseba <wzseba@gmail.com>
2024-11-07 22:52:13 -0300

    add: sobrescritura toString

--- src/parkingUno/Parking.java
+++ src/parkingUno/Parking.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import excepciones.FueraRangoException;
 import excepciones.MatriculaIncorrectaException;
 import excepciones.MatriculaNoExisteException;
 import excepciones.MatriculaRepException;
@@ -13,6 +14,7 @@ public class Parking {
 
 	private List<String> matriculas;
 	private String nombreParking;
+	private int plazasOcupadas;
 
 	public Parking(int tamanio, String nombreParking) {
 		/*
@@ -22,17 +24,26 @@ public class Parking {
 		// inicializando la capacidad interna
 		this.matriculas = new ArrayList<String>(Collections.nCopies(tamanio, null));
 		this.nombreParking = nombreParking;
+		this.plazasOcupadas = 0;
 	}
 
 	public String getNombre() {
-		return nombreParking;
+		return nombreParking.toUpperCase();
+	}
+
+	public void setPlazasOcupadas(int po) {
+		this.plazasOcupadas = po;
 	}
 
 	public void entrada(String matricula, int plaza)
-			throws MatriculaIncorrectaException, PlazaOcuException, MatriculaRepException {
+			throws MatriculaIncorrectaException, PlazaOcuException, MatriculaRepException, FueraRangoException {
 		// Verifica si un objeto es null antes de usarlo
+
 		if (matricula == null || matricula.length() < 4) {
 			throw new MatriculaIncorrectaException("Matricula incorrecta");
+		}
+		if (plaza < 0 || plaza >= matriculas.size()) {
+			throw new FueraRangoException("fuera de rango");
 		}
 		if (matriculas.get(plaza) != null) {
 			throw new PlazaOcuException("Plaza ocupada");
@@ -41,6 +52,7 @@ public class Parking {
 			throw new MatriculaRepException("Matricula repetida");
 		}
 		matriculas.add(plaza, matricula);
+		setPlazasOcupadas(getPlazasOcupadas() + 1);
 	}
 
 	public int salida(String matricula) throws MatriculaNoExisteException {
@@ -50,11 +62,41 @@ public class Parking {
 
 		int plaza = matriculas.indexOf(matricula);
 		matriculas.set(plaza, null);
-
+		setPlazasOcupadas(getPlazasOcupadas() - 1);
 		return plaza;
 	}
 
 	public int getPlazasTotales() {
 		return matriculas.size();
 	}
+
+	public int getPlazasOcupadas() {
+		/*
+		 * Tambien se podria usar foreach pero tendria menor rendimiento si la lista
+		 * llega a ser grande y el metodo es usado con frecuencia. El enfoque actual se
+		 * necesita que se menatenga sincronizado en todos los metodos que sea
+		 * necesario, tiene mejor rendimiento pero se debe ser cuidar el estado del
+		 * atributo plazasOcupadas actualizando en todos los lugares que sea necesario.
+		 */
+		return plazasOcupadas;
+	}
+
+	public int getPlazasLibres() {
+		return getPlazasTotales() - getPlazasOcupadas();
+	}
+
+	@Override
+	public String toString() {
+		String park = "";
+		park = "\t" + getNombre() + "\n----------------------\n";
+		for (int i = 0; i < matriculas.size(); i++) {
+			if (matriculas.get(i) == null) {
+				park += "Plaza " + i + " : (vacia)\n";
+			} else {
+				park += "Plaza " + i + " : " + matriculas.get(i);
+			}
+		}
+		return park;
+	}
+
 }
