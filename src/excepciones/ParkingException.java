732224347183d552abcf87c66939eac331061917
wzseba <wzseba@gmail.com>
2024-11-10 00:43:18 -0300

    add: parkingException

---
+++ src/excepciones/ParkingException.java
@@ -0,0 +1,21 @@
+package excepciones;
+
+public class ParkingException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	private String matri;
+
+	public ParkingException(String msg, String matri) {
+		super(msg);
+		this.matri = matri;
+	}
+
+	public String getMensaje() {
+		return super.getMessage();
+	}
+
+	public String getMatricula() {
+		return matri;
+	}
+}
